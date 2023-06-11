b4ede5fd5bb5a8d9cfd2b0ef6ceef0d4bd42ef3f
leandro16197 <49287681+leandro16197@users.noreply.github.com>
2023-06-11 17:39:54 -0300

    Update Backtracking.java

--- TPE_Prog3/Backtracking.java
+++ TPE_Prog3/Backtracking.java
@@ -36,13 +36,44 @@ public class Backtracking<T> {
         }
     }
 
-    public boolean solucion(Grafo g,ArrayList<Arco> a){
-        boolean verificar=true;
-        
+   private boolean solucion(Grafo g, ArrayList<Arco> tunel) {
+        ArrayList<Vertice<T>> verticesTunel = this.obtenerVertice(tunel);
 
+        // Verificar si la lista está vacía
+        if (verticesTunel.isEmpty()) {
+            return false;
+        }
 
-        return verificar;
+        // Utilizar el primer vértice como vértice inicial para la búsqueda en profundidad
+        Vertice<T> verticeInicial = verticesTunel.get(0);
+
+        // Realizar la búsqueda en profundidad desde el vértice inicial
+        HashSet<Vertice<T>> visitados = new HashSet<>();
+        dfs(g, verticeInicial, visitados);
+
+        // Verificar si todos los vértices en la lista tunel fueron visitados
+        for (Vertice<T> vertice : verticesTunel) {
+            if (!visitados.contains(vertice)) {
+                return false;
+            }
+        }
+
+        return true;
     }
+
+    private void dfs(Grafo g, Vertice<T> vertice, HashSet<Vertice<T>> visitados) {
+        visitados.add(vertice);
+        // Obtener los vértices adyacentes al vértice actual
+        ArrayList<Vertice<T>> adyacentes = g.obtenerAdyacentes(vertice);
+
+        // Recorrer los vértices adyacentes y realizar la búsqueda en profundidad recursivamente
+        for (Vertice<T> adyacente : adyacentes) {
+            if (!visitados.contains(adyacente)) {
+                dfs(g, adyacente, visitados);
+            }
+        }
+    }
+
 
     private ArrayList<Vertice<T>> obtenerVertice(ArrayList<Arco> l) {
         ArrayList<Vertice<T>> aux = new ArrayList<>();
